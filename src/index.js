59322c8e9b9f9caa0009ee1f1b1939471e377ff1
VolodymyrTrykoz <mr.mastachok@gmail.com>
2020-04-06 18:01:24 +0300

    before eject

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/app';
+import { Provider } from 'mobx-react';
+import store from './components/store'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider
+        value={store}
+    >
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

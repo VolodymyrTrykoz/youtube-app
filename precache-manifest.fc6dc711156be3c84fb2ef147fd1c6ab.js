a7b8f1d1d6f36e9f2d820b65ba0f7334d8747ee7
VolodymyrTrykoz <mr.mastachok@gmail.com>
2020-04-07 20:56:01 +0300

    Initial build subtree commit

---
+++ precache-manifest.fc6dc711156be3c84fb2ef147fd1c6ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2909e5504e09d2f9fefb11bf9cf58f04",
+    "url": "/youtube-app/index.html"
+  },
+  {
+    "revision": "864fc6d3a7a7437b6b28",
+    "url": "/youtube-app/static/css/main.312d4857.chunk.css"
+  },
+  {
+    "revision": "8684f2170f7bbc6b1057",
+    "url": "/youtube-app/static/js/2.f9b1dd9b.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/youtube-app/static/js/2.f9b1dd9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "864fc6d3a7a7437b6b28",
+    "url": "/youtube-app/static/js/main.62664dc5.chunk.js"
+  },
+  {
+    "revision": "a7638a1c1dbb157682e1",
+    "url": "/youtube-app/static/js/runtime-main.879a4811.js"
+  }
+]);
